d65beab004d09dc2443b9271c7c0fc5d40fdc2de
Vaibhav Pandey <vaibhav.pandey0806@gmail.com>
2024-02-10 16:18:30 +0530

    changed frontend files

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+import {Button} from "@nextui-org/react";
+function App() {
+  return (
+    <>
+      <Button>Submit</Button>
+    </>
+  )
+}
+
+export default App
--- frontend/vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
-
-export default defineConfig({
-  build: {
-    target: 'esnext',
-  },
-  plugins: [react()],
-});
