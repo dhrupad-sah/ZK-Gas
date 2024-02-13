324fc9bf48a2de11e1bad388af5e8957d5582ef3
Darshan <bennurdarshan@gmail.com>
2024-02-13 18:20:01 +0530

    add layout of polls

--- frontend/src/pages/Polls/MainPoll.jsx
+++ frontend/src/pages/Polls/MainPoll.jsx
@@ -1,12 +1,55 @@
 import { useState } from "react"
+import PollCard from "./PollCard"
 
 export default function MainPoll(){
 
     const [allPolls, setAllPoll] = useState([])
 
-    return(
-        <div>
+    const newPoll1 = {
+        pollID: '#123',
+        pollTitle: 'Which movie will you watch',
+        belongsToCommunity: false,
+        communityID: '',
+        option1: {
+            optionName: "Harry Potter",
+            optionconsensus: 24
+        },
+        option2: {
+            optionName: 'Gone Girl',
+            optionconsensus: 24
+        },
+        option3: {
+            optionName: 'Fantastic Beasts',
+            optionconsensus: 52
+        }
+    }
 
+    const newPoll2 = {
+        pollID: '#456',
+        pollTitle: 'Which Chocolate you like',
+        belongsToCommunity: true,
+        communityID: '#C101',
+        option1: {
+            optionName: "Sneakers",
+            optionconsensus: 32
+        },
+        option2: {
+            optionName: 'Cadbury',
+            optionconsensus: 28
+        },
+        option3: {
+            optionName: '5 Star',
+            optionconsensus: 40
+        }
+    }
+
+    setAllPoll((prevPolls) => [...prevPolls, newPoll1, newPoll2]);
+
+    return (
+        <div>
+          {allPolls.map((poll, index) => (
+            <PollCard key = {index} pollContent = {poll} />
+          ))}
         </div>
-    )
+      );
 }
--- frontend/src/pages/Polls/PollCard.jsx
+++ frontend/src/pages/Polls/PollCard.jsx
@@ -1,33 +1,56 @@
 import { useState } from "react"
+import { LeafPoll, Result } from 'react-leaf-polls'
+import 'react-leaf-polls/dist/index.css'
 
-export default function PollCard(){
+const customTheme = {
+    textColor: 'black',
+    mainColor: '#00B87B',
+    backgroundColor: 'rgb(255,255,255)',
+    alignment: 'center'
+}
+
+export default function PollCard({ pollContent }) {
 
     const [poll, setPoll] = useState({
-        pollID : "",
-        pollTitle : "",
-        isCommunityPoll : false,
-        communityID : "",
-        option1 : {
-            optionName : "",
-            optionconsensus : 0
+        pollID: pollContent.ID,
+        pollTitle: pollContent.title,
+        belongsToCommunity: pollContent.belongsToCommunity,
+        communityID: pollContent.communityID,
+        option1: {
+            optionName: pollContent.option1.optionName,
+            optionconsensus: pollContent.option1.optionconsensus
         },
-        option2 : {
-            optionName : "",
-            optionconsensus : 0
+        option2: {
+            optionName: pollContent.option2.optionName,
+            optionconsensus: pollContent.option2.optionconsensus
         },
-        option3 : {
-            optionName : "",
-            optionconsensus : 0
-        },
-        option4 : {
-            optionName : "",
-            optionconsensus : 0
+        option3: {
+            optionName: pollContent.option3.optionName,
+            optionconsensus: pollContent.option3.optionconsensus
         }
     })
 
-    return(
-        <div>
+    const resData = [
+        { id: 0, text: poll.option1.optionName, votes: poll.option1.optionconsensus },
+        { id: 1, text: poll.option2.optionName, votes: poll.option2.optionconsensus },
+        { id: 2, text: poll.option3.optionName, votes: poll.option3.optionconsensus }
+    ]
 
+    function vote() {
+        console.log("Clicked a Poll")
+    }
+
+    return (
+        <div className="cardContainer" style={{margin : "10px", padding : "5px", border : "1px solid black"}}>
+            <LeafPoll
+                type='multiple'
+                question={poll.pollTitle}
+                results={resData}
+                theme={customTheme}
+                onVote={vote}
+                isVoted={false}
+            />
         </div>
+
     )
 }
