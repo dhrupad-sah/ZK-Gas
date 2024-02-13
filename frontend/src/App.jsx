6459547b655d7858dc8d48c520103cd31b331264
Darshan <bennurdarshan@gmail.com>
2024-02-13 18:51:17 +0530

    add react router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,30 @@
 import NavbarComponent from "./components/Navbar"
 import MainPoll from "./pages/Polls/MainPoll"
+import styled from 'styled-components';
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+
+import Home from "./pages/Home";
+
+
+const Container = styled.div`
+  padding: 0;
+  margin: 0;
+`
 
 export default function App() {
-  return(
-    <div>
-      <NavbarComponent/>
-      <MainPoll/>
-    </div>
-  )  
+  return (
+    <Container>
+      <BrowserRouter>
+        <NavbarComponent />
+        <main>
+          <Routes>
+            <Route path = "/">
+              <Route path = "" element={<Home />}></Route>
+              <Route path = "/polls" element={<MainPoll />}></Route>
+            </Route>
+          </Routes>
+        </main>
+      </BrowserRouter>
+    </Container>
+  )
 }
