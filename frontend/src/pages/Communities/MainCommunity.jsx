ed8a332b1ab8e0bac805da682c051259ddefe6d5
abhishekb740 <abhishekb740@gmail.com>
2024-02-14 23:09:07 +0530

    frontend: Create Community function half done

--- frontend/src/pages/Communities/MainCommunity.jsx
+++ frontend/src/pages/Communities/MainCommunity.jsx
@@ -1,10 +1,18 @@
 import CommunityCard from "../../components/CommunityCard"
 import { Button, useDisclosure, Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, Input, Link, SelectItem, Select } from "@nextui-org/react";
 import { useAuth } from "../../context/auth";
+import { useState } from "react";
 
 export default function MainCommunity() {
     const { auth, setAuth } = useAuth();
     console.log(auth);
+
+    const [communityRules, setCommunityRules] = useState({
+        email: "",
+        region: "",
+        gender: ""
+    });
+
     const communities = [
         {
             communityName: "Anime Society",
@@ -89,17 +97,51 @@ export default function MainCommunity() {
     ]
 
     const { isOpen, onOpen, onOpenChange } = useDisclosure();
-    const handleCreateCommunity = async() => {
-        const community = await auth.FactoryContract.createCommunity(
-            "xx@iiits",
-            "xxxxxxAP",
-            "xxxxxxxM"
-        );
-        console.log(community);
+
+    const handleRulesInput = (e) => {
+        setCommunityRules({
+            ...communityRules,
+            [e.target.name]: e.target.value
+        });
+    }
+
+    const handleCreateCommunity = async () => {
+        console.log(communityRules);
+        // get domain value from email example if email is test@iiits.in get @iiits and make sure it is of length 8, if it is less than 8 then append x before the domain to get its length as 8.
+        const email = communityRules.email;
+        const indexAt = email.indexOf('@');
+        console.log(indexAt);
+        let indexDot = email.length;
+        for (var i = indexAt; i < email.length; i++) {
+            if (email[i] == '.') {
+                indexDot = i;
+                break;
+            }
+        }
+        console.log(email);
+        let domain = email.slice(indexAt + 1, indexDot);
+        console.log(domain);
+        if (domain.length < 8) {
+            let noOfX = 8 - domain.length;
+            console.log(noOfX);
+            for (var j = 0; j < noOfX; j++) {
+                console.log(j);
+                console.log(domain);
+                domain = "x"+domain;
+            }
+            console.log(domain)
+        }
+
+        // const community = await auth.FactoryContract.createCommunity(
+        //     "xx@iiits",
+        //     "xxxxxxAP",
+        //     "xxxxxxxM"
+        // );
+        // console.log(community);
     }
 
     return (
-        <div className="container mx-auto">
+        <div className="container mx-auto" >
             <Button color="danger" fullWidth variant="flat" size="lg" className="mb-3" onPress={onOpen}>
                 Create your own community today!
             </Button>
@@ -125,36 +167,42 @@ export default function MainCommunity() {
                                     label="Email"
                                     placeholder="Enter your email"
                                     variant="bordered"
+                                    name="email"
+                                    onChange={handleRulesInput}
                                 />
                                 <Select
                                     label="Select region"
+                                    name="region"
+                                    onChange={handleRulesInput}
                                 >
-                                    <SelectItem key="asia-acific" value="AP">
+                                    <SelectItem value="asia-pacific" key="xxxxxxAP">
                                         Asia Pacific
                                     </SelectItem>
-                                    <SelectItem key="north-america" value="NA">
+                                    <SelectItem value="north-america" key="xxxxxxNA">
                                         North America
                                     </SelectItem>
-                                    <SelectItem key="europe" value="EU">
+                                    <SelectItem value="europe" key="xxxxxxEU">
                                         Europe
                                     </SelectItem>
-                                    <SelectItem key="middle-east" value="ME">
+                                    <SelectItem value="middle-east" key="xxxxxxME">
                                         Middle East
                                     </SelectItem>
-                                    <SelectItem key="all" value="AL ">
+                                    <SelectItem value="all" key="xxxxxxAL">
                                         All
                                     </SelectItem>
                                 </Select>
                                 <Select
                                     label="Select gender"
+                                    name="gender"
+                                    onChange={handleRulesInput}
                                 >
-                                    <SelectItem key="male" value="M">
+                                    <SelectItem value="male" key="xxxxxxxM">
                                         Male
                                     </SelectItem>
-                                    <SelectItem key="female" value="F">
+                                    <SelectItem value="female" key="xxxxxxxF">
                                         Female
                                     </SelectItem>
-                                    <SelectItem key="both" value="MF">
+                                    <SelectItem value="both" key="xxxxxxMF">
                                         Both
                                     </SelectItem>
                                 </Select>
@@ -163,7 +211,7 @@ export default function MainCommunity() {
                                 <Button color="danger" variant="flat" onPress={onClose}>
                                     Close
                                 </Button>
-                                <Button color="primary" onPress={onClose}>
+                                <Button color="primary" onPress={onClose} onClick={handleCreateCommunity}>
                                     Sign in
                                 </Button>
                             </ModalFooter>
@@ -171,7 +219,7 @@ export default function MainCommunity() {
                     )}
                 </ModalContent>
             </Modal>
-        </div>
+        </div >
 
     );
 }
