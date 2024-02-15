bc13c48b3869bdbca0074b46e44f827ccd065487
Vaibhav Pandey <vaibhav.pandey0806@gmail.com>
2024-02-15 16:43:27 +0530

    update homepage

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,18 @@
-export default function Home(){
-    return(
+import { Image } from "@nextui-org/react";
+import AnonymousGhost from "../assets/anonymous.png";
+
+export default function Home() {
+    return (
         <div>
-            <h1>This is Home Component</h1>
+            <div className="flex justify-around">
+                <div style={{ fontFamily: "Archivo" }}>Hey man</div>
+                <Image
+                    width={400}
+                    alt="Anonymous Ghost"
+                    src={AnonymousGhost}
+                />
+                <div>Hey man</div>
+            </div>
         </div>
     )
 }
