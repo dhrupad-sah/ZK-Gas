6b706db360d977e32e9f2dd5ed2e04926dc77235
abhishekb740 <abhishekb740@gmail.com>
2024-02-15 22:59:18 +0530

    Contracts added in noir-up

--- noir-app/circuits/contract/Factory/Factory.sol
+++ noir-app/circuits/contract/Factory/Factory.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.21;
-import {ZKCommunity} from '../Community/Community.sol';
+pragma solidity ^0.8.18;
+import {ZKCommunity} from "./Community/Community.sol";
 
 contract Factory {
-    uint256 _communityId = 0;
+    uint256 communityId = 0;
 
     address verifier;
 
@@ -18,15 +18,17 @@ contract Factory {
     function createCommunity(
         string memory _domainPub,
         string memory _regionPub,
-        string memory _genderPub
+        string memory _genderPub,
+        string memory _communityName,
+        string memory _communityDescription
     ) public {
-        uint256 _id = _communityId;
+        uint256 _id = communityId;
         address newCommunity = address(
-            new ZKCommunity(_domainPub, _regionPub, _genderPub, verifier, _id)
+            new ZKCommunity(_domainPub, _regionPub, _genderPub, verifier, _id, _communityName, _communityDescription)
         );
         idToCommunity[_id] = newCommunity;
         allCommunities.push(newCommunity);
-        _communityId++;
+        communityId++;
     }
 
     function getCommunity(uint256 _id) public view returns (address) {
