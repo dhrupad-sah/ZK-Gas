8d9278d9755b1457f54f3d654b7806637930d11a
Darshan <bennurdarshan@gmail.com>
2024-02-15 15:23:26 +0530

    add comments in the wall

--- frontend/src/pages/Profile/Profile.jsx
+++ frontend/src/pages/Profile/Profile.jsx
@@ -1,7 +1,10 @@
+import Comments from "./Comments";
+
 export default function Profile(){
     return(
         <div>
-            
+            {/* The below component will be rendered only for the user  */}
+            <Comments/>
         </div>
     )
 }
