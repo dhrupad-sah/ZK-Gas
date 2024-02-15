234aed4a11fa4b10ca76b82d378af0a15f516c3f
Vaibhav Pandey <vaibhav.pandey0806@gmail.com>
2024-02-15 16:43:44 +0530

    global context to metamask

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import NavbarComponent from "./components/Navbar"
 import MainPoll from "./pages/Polls/MainPoll"
 import styled from 'styled-components';
 import { Routes, Route, BrowserRouter } from "react-router-dom";
-
+import { MetaMaskContextProvider } from "./hooks/useMetamask";
 import Home from "./pages/Home";
 import MainCommunity from "./pages/Communities/MainCommunity";
 import CommunityPage from "./pages/Communities/CommunityPage"; // Import the CommunityPage component
@@ -15,22 +15,24 @@ const Container = styled.div`
 
 export default function App() {
   return (
-    <Container>
-      <BrowserRouter>
-        <NavbarComponent />
-        <main>
-          <Routes>
-            <Route path="/">
-              <Route index element={<Home />} /> {/* Use `index` for the default route */}
-              <Route path="polls" element={<MainPoll />} />
-              <Route path="communities" element={<MainCommunity />} />
-              <Route path="communities/:communityId" element={<CommunityPage />} /> {/* Dynamic route */}
+    <MetaMaskContextProvider>
+      <Container>
+        <BrowserRouter>
+          <NavbarComponent />
+          <main>
+            <Routes>
+              <Route path="/">
+                <Route index element={<Home />} /> {/* Use `index` for the default route */}
+                <Route path="polls" element={<MainPoll />} />
+                <Route path="communities" element={<MainCommunity />} />
+                <Route path="communities/:communityId" element={<CommunityPage />} /> {/* Dynamic route */}
 
-              <Route path="profile" element={<Profile/>} />
-            </Route>
-          </Routes>
-        </main>
-      </BrowserRouter>
-    </Container>
+                <Route path="profile" element={<Profile />} />
+              </Route>
+            </Routes>
+          </main>
+        </BrowserRouter>
+      </Container>
+    </MetaMaskContextProvider>
   )
 }
--- frontend/src/pages/Profile/Profile.jsx
+++ frontend/src/pages/Profile/Profile.jsx
@@ -1,6 +1,9 @@
+import { useMetaMask } from "../../hooks/useMetamask";
 import Comments from "./Comments";
 
 export default function Profile(){
+    const { wallet } = useMetaMask();
+    console.log(wallet.accounts[0]);
     return(
         <div>
             {/* The below component will be rendered only for the user  */}
