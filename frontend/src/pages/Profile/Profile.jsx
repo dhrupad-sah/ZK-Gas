bc029a8f408169d58a92f7059d1ba1840762cadd
Vaibhav Pandey <vaibhav.pandey0806@gmail.com>
2024-02-15 16:48:22 +0530

    update profile page

--- frontend/src/pages/Profile/Profile.jsx
+++ frontend/src/pages/Profile/Profile.jsx
@@ -3,13 +3,10 @@ import Comments from "./Comments";
 import Avatar from "../../assets/user_example_avatar.png"
 import { Image, Divider } from "@nextui-org/react"
 import { useAuth } from '../../context/auth.js'
-import { useMetaMask } from "../../hooks/useMetamask";
+import "../../hooks/useMetamask";
 export default function Profile() {
 
     const { wallet } = useMetaMask();
-    console.log(wallet.accounts[0]);
-
-    // Have to replace the Username with : auth.accountAddress
 
     return (
         <div className="container" style={{ display: "flex", flexDirection: "column", padding: "20px" }}>
@@ -18,7 +15,7 @@ export default function Profile() {
                     <Image src={Avatar} alt="User Profile" width={150} height={150} style={{ maxWidth: '100%' }} />
                 </div>
                 <div style={{ flex: "1", display: "flex", flexDirection: "column" }}>
-                    <p style={{ marginBottom: "5px", fontWeight: "bold" }}>Username</p>
+                    <p style={{ marginBottom: "5px", fontWeight: "bold" }}>{wallet.accounts[0]}</p>
                     <Divider style={{ borderTop: "2px solid #9e9d9d", width: "100%" }} />
                 </div>
             </div>
