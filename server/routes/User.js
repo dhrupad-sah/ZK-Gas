95088b9bad77516ed2901a0a76cd387248a14a3f
Darshan <bennurdarshan@gmail.com>
2024-02-16 20:57:49 +0530

    add metamask to mongo Instance

--- server/routes/User.js
+++ server/routes/User.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router();
-const { getAllUsers, createUser, addCommentForUser, getAllCommentsOfUser } = require('../controllers/User.js');
+const { getAllUsers, createUser, addCommentForUser, getAllCommentsOfUser, getMongoIDUsingMetamaskID } = require('../controllers/User.js');
 
 //GET Routes : 
 router.get('/getAllUsers', getAllUsers);
@@ -9,5 +9,6 @@ router.get('/getAllUsers', getAllUsers);
 router.post('/createUser', createUser);
 router.post('/addCommentForUser', addCommentForUser)
 router.post('/getAllCommentsOfUser', getAllCommentsOfUser)
+router.post('/getMongoDbIdUsingMetamask', getMongoIDUsingMetamaskID)
 
 module.exports = router
