88ba436c997aa59b16872ba977ab55461de4888d
Darshan <bennurdarshan@gmail.com>
2024-02-17 03:36:27 +0530

    add poll id to user

--- server/routes/User.js
+++ server/routes/User.js
@@ -1,16 +1,20 @@
 const express = require('express')
 const router = express.Router();
-const { getAllUsers, createUser, addCommentForUser, getAllCommentsOfUser, getMongoIDUsingMetamaskID, addCommunityOfUser, getAllCommunityOfUser } = require('../controllers/User.js');
+const { getAllUsers, createUser, addCommentForUser, getAllCommentsOfUser, getMongoIDUsingMetamaskID, addCommunityOfUser, getAllCommunityOfUser, addPollForUser } = require('../controllers/User.js');
 
 //GET Routes : 
 router.get('/getAllUsers', getAllUsers);
 
 //POST Routes : 
 router.post('/createUser', createUser);
+router.post('/getMongoDbIdUsingMetamask', getMongoIDUsingMetamaskID)
+
 router.post('/addCommentForUser', addCommentForUser)
 router.post('/getAllCommentsOfUser', getAllCommentsOfUser)
-router.post('/getMongoDbIdUsingMetamask', getMongoIDUsingMetamaskID)
+
 router.post('/addCommunityForUser', addCommunityOfUser)
 router.post('/getAllCommunityOfUser', getAllCommunityOfUser)
+
+router.post('/addPollIdToUser', addPollForUser)
 
 module.exports = router
