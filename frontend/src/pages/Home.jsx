587c68a5067b67b37dba57fab9e4b9bd07912dfa
dhrupad-sah <thedhrupad@gmail.com>
2024-02-18 03:31:06 +0530

    update homepage

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,18 +1,47 @@
 import { useEffect, useRef } from "react";
-import { Image, Divider, Link } from "@nextui-org/react";
+import { Button, Image } from "@nextui-org/react";
 import styled from "styled-components";
 import BackgroundImage1 from "./Home/3.png";
+import Anonymous from "../assets/anonymous.png";
 import "./styles/Home.css";
+import Atropos from "atropos/react";
 
 export default function Home() {
-    return (
-        <div>
-            <h1 className="text-6xl grad">Empower Identity, Protect Privacy</h1>
-            <h1 className="text-6xl grad">Build Communities</h1>
-        </div>
-    )
+  return (
+    <div className="pt-28">
+      <h1 className="text-[73px] grad text-center">
+        Empower Identity, Protect Privacy
+      </h1>
+      <h1 className="text-[73px] grad text-center">Build Communities</h1>
+      <div className="flex justify-center gap-[200px] p-10">
+        <Button
+          radius="full"
+          variant="bordered"
+          className="w-[15rem] h-[4rem] px-15 font-semibold border-[#D94FD5]"
+        >
+          <span className="text-white text-[20px] text-grad ">
+            Create Community
+          </span>
+        </Button>
+        <Button
+          radius="full"
+          variant="bordered"
+          className="w-[15rem] h-[4rem] text-[20px] font-semibold border-[#D94FD5]"
+        >
+          <span className="text-grad-rev">Create Poll</span>
+        </Button>
+      </div>
+      <div className="flex gap-[900px] pt-56">
+        <div>sup nigga</div>
+        <Atropos className="my-atropos" shadow={false} highlight={false}>
+          <Image
+            isBlurred
+            style={{ width: "90%", height: "100%" }}
+            alt="Home Image"
+            src={Anonymous}
+          />
+        </Atropos>
+      </div>
+    </div>
+  );
 }
-
-
-
-
