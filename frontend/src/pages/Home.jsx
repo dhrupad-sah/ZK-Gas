7184ab1299f262de02d54f030c80a0827bb7917d
abhishekb740 <abhishekb740@gmail.com>
2024-02-18 06:38:19 +0530

    minor changes

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -14,7 +14,7 @@ export default function Home() {
   const lines_1 = ["Protect Privacy, Build Communities"];
 
   return (
-    <div className="flex-col" style={{ minHeight: '200vh', background: 'linear-gradient(to bottom, #1b1521, #47193d)'}}>
+    <div className="flex-col" style={{ minHeight: '200vh', background: 'linear-gradient(to bottom, #1b1521, #47193d)' }}>
       <div style={{ display: 'flex', flexDirection: 'column', gap: '8rem', minHeight: '80vh', paddingTop: '8rem' }}>
         <div style={{ display: 'flex', width: '100%', justifyContent: 'center', height: '7rem' }}>
           <div style={{ width: '75%' }}>
